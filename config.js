e101c509df97f4d53d02fdf61053ae0841be34df
mahlorisaint-arch <mahlorisaint@gmail.com>
2025-10-13 03:14:31 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
-
+export const SUPABASE_URL = "https://ydnxqnbjoshvxteevemc.supabase.co";
+export const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlkbnhxbmJqb3Nodnh0ZWV2ZW1jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc2MzI0OTMsImV4cCI6MjA3MzIwODQ5M30.NlaCCnLPSz1mM7AFeSlfZQ78kYEKUMh_Fi-7P_ccs_U"
